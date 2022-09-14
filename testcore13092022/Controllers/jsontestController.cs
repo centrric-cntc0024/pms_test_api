235c54fc04e06a5684339f60f57efbb410544f9c
Bhagya <cntc0024@centrric.com>
2022-09-14 08:10:14 -0700

    intial commit

---
+++ testcore13092022/Controllers/jsontestController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace testcore13092022.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class jsontestController : ControllerBase
+    {
+       [HttpGet]
+       [Route("jsontest")]
+        public dynamic testjson()
+        {
+            var data = new List<Root>();
+
+
+        
+            using (StreamReader r = new StreamReader("test.json"))
+            {
+                string json = r.ReadToEnd();
+                data = JsonSerializer.Deserialize<List<Root>>(json);
+                Console.WriteLine("dataaaaa.....", data);
+            }               
+            //string h="";
+            //List<string> itemOUT = new List<string>();
+            //List<string> eve = new List<string>();
+            //List<string> datas = new List<string>();
+
+            //string eventkey = "sec_web-usr-rqst_rjct_scs";
+            //string j = "";
+            //string frond = "";
+            //string msg = "";
+            //string desc = "";
+            
+            if (data != null && data.Count>0)
+            {
+                List<Root> datazz = data.FindAll(i => i.sub_module_name == "Roles" && i.events!=null  );
+                List<Event> data1 = datazz[0].events.FindAll(x => x.event_key == "sec_rol_crt_scs");
+
+                return data1;
+                //var ddd = datazz.FindAll(i => i.ev);
+
+                //foreach (var check in data)
+                //{
+
+
+                //    h = check.events.ToString();
+
+                //    var evnts = check.events;
+                //    Console.WriteLine("output..........", h);
+
+                //    Console.WriteLine("output..........", check.events);
+                //    string ss = "";
+                //    foreach (var d in evnts)
+                //    {
+                //        ss = d.event_name;
+                //        if(d.event_key==eventkey)
+                //        {
+                //            j = d.event_key;
+                //            frond=d.frontend_key;
+                //            msg = d.message;
+                //            desc = d.event_description;
+                //            datas.Add(desc);
+                //            datas.Add(msg);
+                //            datas.Add(frond);
+
+
+                //        }
+                                               
+                //    }
+               //itemOUT.Add(datazz);
+                    
+                //}
+                //eve.Add(h);
+
+              
+
+            }
+
+            return "tdfgd";
+        }
+
+
+
+
+        public class Event
+        {
+            public string event_name { get; set; }
+            public string event_key { get; set; }
+            public string message { get; set; }
+            public string event_description { get; set; }
+            public string message_ar { get; set; }
+            public string event_description_ar { get; set; }
+            public string frontend_key { get; set; }
+            public bool success { get; set; }
+            public List<Reason> reasons { get; set; }
+            public string event_descriptioon { get; set; }
+        }
+
+        public class Reason
+        {
+            public string reson_id { get; set; }
+            public string reason { get; set; }
+        }
+
+        public class Root
+        {
+            public string module_id { get; set; }
+            public string module { get; set; }
+            public string sub_module_id { get; set; }
+            public string sub_module_name { get; set; }
+            public List<Event> events { get; set; }
+        }
+
+
+    }
+
+}
---
+++ testcore13092022/Controllers/testController.cs
@@ -0,0 +1,238 @@
+﻿using Konscious.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testcore13092022.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        private readonly ILogger<testController> logger;
+
+        public testController(ILogger<testController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        #region GetCustomer
+
+        [HttpGet]
+        [Route("GetCustomer")]
+        public string Get()
+        {
+            logger.LogInformation("this is the first log msg");
+            logger.LogWarning("this is warning");
+
+            Console.WriteLine("hello");
+
+
+            return "Hello world!!!!!!";
+        }
+        #endregion
+
+
+        //[HttpGet]
+        //[Route("hashi")]
+        //public string Gett()
+        //{
+
+        //    var buffer = new byte[16];
+        //    var rng = new RNGCryptoServiceProvider();
+
+        //    rng.GetBytes(buffer);
+        //    var salt = buffer;
+        //    Console.WriteLine("salt is", Convert.ToBase64String(salt));
+
+
+        //    var password = "hiii";
+        //    var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+        //    argon2.Salt = salt;
+        //    argon2.DegreeOfParallelism = 8; // four cores
+        //    argon2.Iterations = 4;
+        //    argon2.MemorySize = 1024 * 1024; // 1 GB
+        //    var hash = argon2.GetBytes(16);
+        //    Console.WriteLine($"Hash is '{ Convert.ToBase64String(hash) }'.");
+
+        //    var h = argon2.GetBytes(16);
+        //    return "success";
+
+        //}
+
+        //[HttpGet]
+        //[Route("verifyy")]
+        //public string Gett()
+        //{
+
+        //}
+
+
+        //private string test()
+        //{
+        //    var buffer = new byte[16];
+        //    var rng = new RNGCryptoServiceProvider();
+        //    Console.WriteLine("salt is", rng);
+        //    rng.GetBytes(buffer);
+        //    var salt = buffer;
+
+
+        //    var password = "hiii";
+        //    var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+        //    argon2.Salt = salt;
+        //    argon2.DegreeOfParallelism = 8; // four cores
+        //    argon2.Iterations = 4;
+        //    argon2.MemorySize = 1024 * 1024; // 1 GB
+        //    var hash = argon2.GetBytes(16);
+        //    Console.WriteLine($"Hash is '{ Convert.ToBase64String(hash) }'.");
+
+        //    var h = argon2.GetBytes(16);
+        //    return "success";
+
+        //}
+
+        public string salt()
+        {
+            var buffer = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            Console.WriteLine("salt is", rng);
+            rng.GetBytes(buffer);
+            var saltt = Convert.ToBase64String(buffer);
+            Console.WriteLine($"var saltt is '{ Convert.ToBase64String(buffer) }'.");
+
+            Console.WriteLine($"Using salt in createsalt '{ Convert.ToBase64String(buffer) }'.");
+
+            return saltt;
+        }
+        public string passwordd()
+        {
+            var pass = "hello";
+            return pass;
+        }
+        [HttpGet]
+        [Route("hashing")]
+        public string HashPassword()
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(passwordd()));
+            var salttt = Encoding.UTF8.GetBytes("tyoWpm8iM5hpMLFn/Vh+wQ==");
+            argon2.Salt = salttt;
+            
+            Console.WriteLine($"Using  argon salt  '{ Convert.ToBase64String(argon2.Salt) }'.");
+
+            argon2.DegreeOfParallelism = 8; // four cores
+            argon2.Iterations = 4;
+            argon2.MemorySize = 1024 * 1024; // 1 GB
+            var hashh = Convert.ToBase64String(argon2.GetBytes(16));
+           
+            Console.WriteLine($"Hash is '{ Convert.ToBase64String(argon2.GetBytes(16)) }'.");
+            return hashh;
+        }
+
+        
+
+        [HttpGet]
+        [Route("verifyhashh")]
+
+        public bool VerifyHash()
+        
+        {
+            
+           
+            string pwd = passwordd();
+            var newHash =HashPassword();
+            var hash = "y4WOQlv6PdDytLdEx4fXqw==";
+            var result= hash.SequenceEqual(newHash);
+
+            return result;
+        }
+        //[HttpGet]
+        //[Route("Get")]
+        //public void Run()
+        //{
+           
+        //    var stopwatch = Stopwatch.StartNew();
+
+        //    Console.WriteLine($"Creating hash for password '{ password }'.");
+
+        //    var salt = CreateSalt();
+        //    Console.WriteLine($"Using salt '{ Convert.ToBase64String(salt) }'.");
+
+        //    var hash = HashPassword(password, salt);
+        //    Console.WriteLine($"Hash is '{ Convert.ToBase64String(hash) }'.");
+
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"Process took { stopwatch.ElapsedMilliseconds / 1024.0 } s");
+
+        //    stopwatch = Stopwatch.StartNew();
+        //    Console.WriteLine($"Verifying hash...");
+
+        //    var success = VerifyHash(password, salt, hash);
+        //    Console.WriteLine(success ? "Success!" : "Failure!");
+
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"Process took { stopwatch.ElapsedMilliseconds / 1024.0 } s");
+        //}
+        //[HttpGet]
+        //[Route("verifyy")]
+        //private bool VerifyHash(string password, byte[] salt, byte[] hash)
+        //{
+        //    var newHash = HashPassword(password, salt);
+        //    return hash.SequenceEqual(newHash);
+        //}
+        //[HttpGet]
+        //[Route("Geting")]
+        //public string verify()
+        //{
+        //    var password = "Hello World!";
+        //    var stopwatch = Stopwatch.StartNew();
+
+        //    Console.WriteLine($"Creating hash for password '{ password }'.");
+
+        //    var salt = CreateSalt();
+        //    Console.WriteLine($"Using salt '{ Convert.ToBase64String(salt) }'.");
+
+        //    var hash = HashPassword(password, salt);
+        //    Console.WriteLine($"Hash is '{ Convert.ToBase64String(hash) }'.");
+
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"Process took { stopwatch.ElapsedMilliseconds / 1024.0 } s");
+
+        //    stopwatch = Stopwatch.StartNew();
+        //    Console.WriteLine($"Verifying hash...");
+
+        //    var success = VerifyHash(password, salt, hash);
+        //    Console.WriteLine(success ? "Success!" : "Failure!");
+
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"Process took { stopwatch.ElapsedMilliseconds / 1024.0 } s");
+        //}
+
+
+
+        //[HttpGet]
+        //[Route("verifyy")]
+        //public bool VerifyHash(string password, byte[] salt, byte[] hash)
+        //{
+        //    var success = VerifyHash(password, salt, hash);
+        //    Console.WriteLine(success ? "Success!" : "Failure!");
+
+        //    stopwatch.Stop();
+        //    Console.WriteLine($"Process took { stopwatch.ElapsedMilliseconds / 1024.0 } s");
+
+
+        //}
+
+
+
+    }
+}
